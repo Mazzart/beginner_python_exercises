3d1cfe49727fb96bf3b00797f0a5ed3a38753507
Artem Mazur <am.magicsmile@gmail.com>
2018-09-01 12:00:22 +0300

    Create 05_list_overlap.py

---
+++ 05_list_overlap.py
@@ -0,0 +1,9 @@
+"""
+Take two lists and write a program that returns a list that contains only the elements that are common 
+between the lists (without duplicates). Make sure your program works on two lists of different sizes.
+
+Extras:
+
+1) Randomly generate two lists to test this
+2) Write this in one line of Python
+"""
