6651cc07dd9d906b92082b92c2ff7cb3ec2a43ff
mazart <mazart@MacBook-Pro-apple.local>
2018-09-12 21:53:02 +0300

    Add solution to the file, but not final (rewrite like a function and ...)

--- 08_rock_paper_scissors.py
+++ 08_rock_paper_scissors.py
@@ -13,14 +13,21 @@ Paper beats rock
 
 while True:
     game = input("Do you want to play? yes/no: ")
-    if game == 'yes':
+    if game == "yes":
         print("We are starting Rock-Paper-Scissors game.")
         player_1 = input("First player enter your choice: ")
         player_2 = input("Second player enter your choice: ")
-        player_3 = input("Third player enter your choice: ")
-        p_1, p_2, p_3 = player_1.lower(), player_2.lower(), player_3.lower()
-        if p_1 == p_2 and p_1 == p_3:
+        p_1, p_2 = player_1.lower(), player_2.lower()
+        if p_1 == p_2:
             print("It is a draw.")
-    else:
+        elif ((p_1 == "rock" and p_2 == "scissors") or
+             (p_1 == "scissors" and p_2 == "paper") or
+             (p_1 == "paper" and p_2 == "rock")):
+            print("Player 1 win.")
+        else:
+            print("Player 2 win.")
+    elif game == "no":
         print("OK, see you later.")
         break
+    else:
+        print("Invalid input. Try again.")
