4fe5d593e353a02845326ed146e0cfe860caa488
mazart <mazart@MacBook-Pro-apple.local>
2018-09-15 23:36:32 +0300

    Add recursion solution

--- 06_string_lists.py
+++ 06_string_lists.py
@@ -9,3 +9,28 @@ if word == word[::-1]:
     print("{} is a palindrome".format(word))
 else:
     print("{} is not a palindrome".format(word))
+
+
+# Solution with recursion
+def is_palindrome(s):
+
+    def to_chars(s):
+        s = s.lower()
+        ans = ''
+        for char in s:
+            if char in "abcdefghijklmnopqrstuvwxyz":
+                ans = ans + char
+        return ans
+
+    def is_pal(s):
+        if len(s) <= 1:
+            return True
+        else:
+            return s[0] == s[-1] and is_pal(s[1:-1])
+
+    return is_pal(to_chars(s))
+
+
+print(is_palindrome("Hello, olleh"))
+print(is_palindrome("Elba non able"))
+print(is_palindrome("Hello world!"))
