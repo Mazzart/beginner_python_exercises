60632c991d91b901c03d329e898a5823c99bd969
mazart <mazart@MacBook-Pro-apple.local>
2018-09-26 06:07:30 +0300

    Solution for the exercise number 10

--- 10_list_overlap_comprehensions.py
+++ 10_list_overlap_comprehensions.py
@@ -6,6 +6,17 @@ Make sure your program works on two lists of different sizes.
 Extra:
 Randomly generate two lists to test this
 """
+import random
 
-a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
-b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+first_list = random.sample(range(100), 15)  # randomly generates list of len 10
+second_list = random.sample(range(100), 20)  # randomly generates list of len 15
+
+
+def common_items(a: list, b: list) -> list:
+    """
+    Return list of common items in two lists
+    """
+    return [i for i in set(a) if i in b]
+
+
+print(common_items(first_list, second_list))
