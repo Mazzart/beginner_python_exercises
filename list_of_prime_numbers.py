33c5182fc2f8d608f1b93baa54ce7d89d0c9dad7
mazart <mazart@MacBook-Pro-apple.local>
2018-10-07 00:17:49 +0300

    Add solution

--- list_of_prime_numbers.py
+++ list_of_prime_numbers.py
@@ -1,1 +1,23 @@
+import math
+
+while True:
+    try:
+        n = int(input("Please, enter positive integer number: "))
+        break
+    except ValueError:
+        print("Please, try again...")
+
+
+def prime_number(n):
+    if n == 1:
+        return False
+
+    divisor = math.floor(math.sqrt(n))
+    for d in range(2, divisor+1):
+        if n % d == 0:
+            return False
+    return True
+
+result = [i for i in range(1, n+1) if prime_number(i)]
+print(result)
 
