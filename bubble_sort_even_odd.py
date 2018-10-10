a60a9a13f19495f68a16844e00c7fab8d42d28a2
mazart <mazart@MacBook-Pro-apple.local>
2018-10-10 19:08:04 +0300

    Add solution

--- bubble_sort_even_odd.py
+++ bubble_sort_even_odd.py
@@ -1,1 +1,32 @@
+import random
 
+while True:
+    try:
+        n = int(input('Enter the length of the list (positive integer): '))
+        items_list = random.sample(range(n*10), n)
+        break
+    except ValueError:
+        print('Please, try again...')
+
+even, odd = [], []
+
+for item in items_list:
+    if item % 2 == 0:
+        even.append(item)
+    else:
+        odd.append(item)
+
+def bubble_sort(items_list: list) -> list:
+    for i in range(len(items_list)):
+        swapped = False
+        for j in range(len(items_list)-1):
+            if items_list[j] > items_list[j+1]:
+                items_list[j], items_list[j+1] = items_list[j+1], items_list[j]
+                swapped = True
+        if not swapped:
+            break
+    return items_list
+
+bubble_sort(even)
+bubble_sort(odd)
+print(even + odd)
