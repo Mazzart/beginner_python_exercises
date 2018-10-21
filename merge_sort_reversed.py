dc8095b8449cadaaae9608b9a9ab1bccec16ffd3
mazart <mazart@MacBook-Pro-apple.local>
2018-10-21 22:31:33 +0300

    Add solution

--- merge_sort_reversed.py
+++ merge_sort_reversed.py
@@ -1,1 +1,30 @@
-m
+def merge_sort(left, right):
+    result = []
+    i, j = 0, 0
+    while i < len(left) and j < len(right):
+        if left[i] > right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+    while (i < len(left)):
+        result.append(left[i])
+        i += 1
+    while (j < len(right)):
+        result.append(right[j])
+        j += 1
+    return result
+
+
+def merge_sort_recur(L):
+    if len(L) < 2:
+        return L[:]
+    else:
+        middle = len(L) // 2
+        left = merge_sort_recur(L[:middle])
+        right = merge_sort_recur(L[middle:])
+        return merge_sort(left, right)
+
+
+print(merge_sort_recur([1, 100, 23, 56, 43, 9, 5, 16, 67, 32]))
