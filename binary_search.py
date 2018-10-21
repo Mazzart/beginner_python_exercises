bbc8c72e18f34ed8cf5b73a6cc61efaa8db61659
mazart <mazart@MacBook-Pro-apple.local>
2018-10-21 22:21:22 +0300

    Add solution 2

--- binary_search.py
+++ binary_search.py
@@ -8,6 +8,7 @@ while True:
     except ValueError:
         print('Please, try again...')
 
+# Solution 1
 def binary_search(list: list, value: int) -> int:
     '''Search algorithm that finds the positions of a target value'''
     list.sort() # binary search works for sorted list
@@ -23,3 +24,24 @@ def binary_search(list: list, value: int) -> int:
             else:
                 return mid
     return -1
+
+# Solution 2 (is number in list)
+def bisect_search(L: list, e: int) -> bool:
+    L.sort() # binary search works for sorted list
+    def bisect_search_helper(L: list, e: int, low: int, high: int) -> bool:
+        if high == low:
+            return L[low] == e
+        mid = (low + high) // 2
+        if L[mid] == e:
+            return True
+        elif L[mid] > e:
+            if low == mid: # nothing left to search
+                return False
+            else:
+                return bisect_search_helper(L, e, low, mid-1)
+        else:
+            return bisect_search_helper(L, e, mid+1, high)
+    if len(L) == 0:
+        return False
+    else:
+        return bisect_search_helper(L, e, 0, len(L)-1)
