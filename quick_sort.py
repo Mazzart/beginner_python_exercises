214ddddab772b6ebef07787b38e17a9118f24abb
mazart <mazart@MacBook-Pro-apple.local>
2018-10-22 05:16:36 +0300

    Add solution

--- quick_sort.py
+++ quick_sort.py
@@ -1,1 +1,9 @@
-q
+def quick_sort(list_num: list) -> list:
+    '''Return a sorted list in ascending order'''
+    if len(list_num) < 2:
+        return list_num
+    else:
+        pivot = list_num[0]
+        less_part = [i for i in list_num[1:] if i <= pivot]
+        greater_part = [i for i in list_num[1:] if i > pivot]
+        return quick_sort(less_part) + [pivot] + quick_sort(greater_part)
