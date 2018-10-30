e3829d7408ab09f03f2c4923a77599507321468b
mazart <mazart@MacBook-Pro-apple.local>
2018-10-30 21:08:17 +0200

    From single to double quotes, some fixes

--- sort_freq_desc_order.py
+++ sort_freq_desc_order.py
@@ -2,17 +2,17 @@ import random
 from collections import Counter
 
 
-def sort_freq_desc_order(list: list) -> list:
-    '''Sorts the list by the frequency of entry in a descending order'''
-    c = Counter(list)
+def sort_freq_desc_order(array: list) -> list:
+    """Sorts the list by the frequency of entry in a descending order"""
+    c = Counter(array)
     result = []
 
-    sorted_by_value = sorted(c.items(), key=lambda kv:kv[1], reverse=True)
+    sorted_by_value = sorted(c.items(), key=lambda kv: kv[1], reverse=True)
     for item in sorted_by_value:
-        result.extend([item[0] for i in range(item[1])])
+        result.extend([item[0] for _ in range(item[1])])
     return result
 
 
 if __name__ == '__main__':
-    print(sort_freq_desc_order([4,3,7,4,3,4,2,7,5,4,5,7]))
-    print(sort_freq_desc_order([random.randint(1,10) for i in range(20)]))
+    print(sort_freq_desc_order([4, 3, 7, 4, 3, 4, 2, 7, 5, 4, 5, 7]))
+    print(sort_freq_desc_order([random.randint(1, 10) for i in range(20)]))
