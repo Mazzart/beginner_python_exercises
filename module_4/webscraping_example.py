f23125b03edab41519e0ae075410cdafe0ad36d1
Mazzart <mazart@MacBook-Pro-apple.local>
2018-11-14 06:19:58 +0200

    Add code to provide information about the country the user asks

--- module_4/webscraping_example.py
+++ module_4/webscraping_example.py
@@ -16,13 +16,13 @@ def get_page_from_server(page_url: str) -> str:  # getting page from server
     return str(page)
 
 
-def get_tags_info(tag: str, page: str) -> list:
+def get_tags_info(tag: str, page: str, additional_characters=0) -> list:
     """Return information that has a tag"""
 
     # finding the place where tag is located
     # the tag for each country is the same but the last 11 characters are different
     # 11 - is a number of characters to reach the country name
-    tag_size = len(tag) + 11
+    tag_size = len(tag) + additional_characters
     tag_count = page.count(tag)  # counts the number of times tag appears on the page
     countries_list = []
 
@@ -48,12 +48,37 @@ def get_tags_info(tag: str, page: str) -> list:
 example_page = get_page_from_server(base_url)
 COUNTRY_TAG = '<img src="/places/static/images/flags/'
 # list of countries from the first page
-countries = get_tags_info(COUNTRY_TAG, example_page)
+countries = get_tags_info(COUNTRY_TAG, example_page, 11)  # third argument is additional characters
+countries_dict = {'0': countries}
 
 # iterates through pages and adds countries on the page to the list of countries
 for page_number in range(1, 26):
     next_page = base_url + str(page_number)
     example_page = get_page_from_server(next_page)
-    countries.extend(get_tags_info(COUNTRY_TAG, example_page))
+    current_countries = get_tags_info(COUNTRY_TAG, example_page, 11)  # third argument is additional characters
+    countries.extend(current_countries)
+    countries_dict[str(page_number)] = current_countries
 
 print(countries)
+
+country_name = input("Enter the name of the country from the previous list: ")
+
+# finds on what page is the country that the user asks
+for page_number, country_list in countries_dict.items():
+    if country_name in country_list:
+        next_page = base_url + page_number
+
+country_index = countries.index(country_name) + 1  # finds position of the user country in the list of countries
+country_name = country_name.replace(' ', '-')  # convert name of the country to the url format
+# country page for getting information
+country_url = 'http://example.webscraping.com/places/default/view/' + country_name + str(country_index)
+country_page = get_page_from_server(country_url)
+
+COUNTRY_TAG_INFO = '<td class="w2p_fw">'
+country_info = get_tags_info(COUNTRY_TAG_INFO, country_page)
+
+print(f"Information about {country_name.replace('-', ' ').upper()}\n")
+print(f"Area: {country_info[1]}")
+print(f"Population: {country_info[2]}")
+print(f"Capital: {country_info[5]}")
+print(f"Currency name: {country_info[9]}")
