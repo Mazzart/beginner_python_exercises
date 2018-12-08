e6ae45d090234d8cfd18ae0f872dddf3021fbb43
Mazzart <am.magicsmile@gmail.com>
2018-12-08 21:09:19 +0200

    Solution for Task 3

--- module_5/re_min.py
+++ module_5/re_min.py
@@ -6,18 +6,24 @@ import re
 # Task 1 - Say hello to a user using the typed name
 # --------------------------------------------------------------------
 # Format for answer_1: my name is ... or ... is my name
-answer_1 = input('What is your name? ')
+#answer_1 = input('What is your name? ')
 # name =
-print(f'Hello {name}.')
+#print(f'Hello {name}.')
 # --------------------------------------------------------------------
 # Task 2 - Check email
 # --------------------------------------------------------------------
-answer_2 = input('Enter your email address: ')
+#answer_2 = input('Enter your email address: ')
 # --------------------------------------------------------------------
 # Task 3 - Check phone number
 # --------------------------------------------------------------------
 # Format for the phone number: (xxx) xxx - xxxx
 answer_3 = input('Enter your phone number: ')
+phone_number_regex = re.compile(r'\(\d\d\d\)\s\d\d\d\s-\s\d\d\d\d')
+try:
+    phone_number = phone_number_regex.search(answer_3)
+    print(f'Your phone number is: {phone_number.group()}')
+except AttributeError:
+    print('Please, enter your phone number in the correct format.')
 # --------------------------------------------------------------------
 # Task 4 - Find tag <title> in the file and print text inside
 # --------------------------------------------------------------------
