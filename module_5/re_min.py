333ecbc2dd620d2a97c49b4c2a6a2373148a6ba6
Mazzart <am.magicsmile@gmail.com>
2018-12-08 20:57:27 +0200

    Add description for tasks.

---
+++ module_5/re_min.py
@@ -0,0 +1,23 @@
+"""Practice of regular expressions"""
+
+import re
+
+# --------------------------------------------------------------------
+# Task 1 - Say hello to a user using the typed name
+# --------------------------------------------------------------------
+# Format for answer_1: my name is ... or ... is my name
+answer_1 = input('What is your name? ')
+# name =
+print(f'Hello {name}.')
+# --------------------------------------------------------------------
+# Task 2 - Check email
+# --------------------------------------------------------------------
+answer_2 = input('Enter your email address: ')
+# --------------------------------------------------------------------
+# Task 3 - Check phone number
+# --------------------------------------------------------------------
+# Format for the phone number: (xxx) xxx - xxxx
+answer_3 = input('Enter your phone number: ')
+# --------------------------------------------------------------------
+# Task 4 - Find tag <title> in the file and print text inside
+# --------------------------------------------------------------------
