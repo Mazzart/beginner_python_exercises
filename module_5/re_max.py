6f3fce0f41f89ba1d3d781905631ae525eb071d5
Mazzart <am.magicsmile@gmail.com>
2018-12-10 05:29:03 +0200

    Add description of the program.

---
+++ module_5/re_max.py
@@ -0,0 +1,10 @@
+"""Practice of regular expressions
+
+Write the program to check the password security.
+1) should contain [a-z], [A-Z], [0-9] - one from each group
+2) should contain [*#+@]
+3) length from 4 to 6 symbols (without spaces)
+"""
+
+import re
+
