ce4e0ae7ae502c38c9ea0f4d13b1c4ca9cc63191
Mazzart <am.magicsmile@gmail.com>
2019-12-08 14:49:35 +0200

    Add quicksort example from book

--- quick_sort.py
+++ quick_sort.py
@@ -1,6 +1,6 @@
 def quick_sort(list_num: list) -> list:
-    '''Return a sorted list in ascending order'''
-    
+    """Return a sorted list in ascending order"""
+
     if len(list_num) < 2:
         return list_num
     else:
@@ -8,3 +8,16 @@ def quick_sort(list_num: list) -> list:
         less_part = [i for i in list_num[1:] if i <= pivot]
         greater_part = [i for i in list_num[1:] if i > pivot]
         return quick_sort(less_part) + [pivot] + quick_sort(greater_part)
+
+
+def quicksort(lst: list) -> list:
+    """Quicksort over a list-like sequence"""
+
+    if len(lst) == 0:
+        return lst
+
+    pivot = lst[0]
+    pivots = [x for x in lst if x == pivot]
+    small = quicksort([x for x in lst if x < pivot])
+    large = quicksort([x for x in lst if x > pivot])
+    return small + pivots + large
